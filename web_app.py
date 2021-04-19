715e0f433e5e4be2d15f4cfcb6a209795f5e62f2
Tia <49786642+soft-nougat@users.noreply.github.com>
2021-04-19 20:01:57 +0200

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -96,9 +96,19 @@ try:
     
     # set bg
     #set_png_as_page_bg("app_bg.png")
-    set_png_as_page_bg('background.png')
-
+    #set_png_as_page_bg('background.png')
     
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+    background-size: cover;
+    }
+    </style>
+    '''
+
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
     # Main panel setup
     display_app_header(main_txt='YS Community CleanUp',
                        sub_txt='Clean up your community!')
