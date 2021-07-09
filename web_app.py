cc84a912b5b529eba4697cd2faf786d00d535dad
Tia <49786642+soft-nougat@users.noreply.github.com>
2021-07-09 10:54:15 +0200

    Add client settings for webrtc

--- web_app.py
+++ web_app.py
@@ -23,6 +23,10 @@ from tempfile import NamedTemporaryFile
 import os 
 from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
+WEBRTC_CLIENT_SETTINGS = ClientSettings(
+    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+    media_stream_constraints={"video": True, "audio": True},
+)
 
 def main():
     set_bg_hack()
